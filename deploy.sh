5c593d05b3a9e9db84acec2e44aee92d7a2e697a
shuwoom <shuwoom.wgc@gmail.com>
2024-11-01 09:37:35 +0800

    v1.0

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:guanchao/workflow-engine-book.git main:pages
+git push -f git@github.com:botmancc/botman-web.git main:pages
 
 cd -
